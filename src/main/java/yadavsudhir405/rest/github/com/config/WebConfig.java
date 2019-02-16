6ecccf13ebed1560eaf2c4bf9888eff293869320
Sudhir Yadav <yadavsudhir405@gmail.com>
2019-02-16 21:02:55 +0530

    Added the custom static resource location

--- src/main/java/yadavsudhir405/rest/github/com/config/WebConfig.java
+++ src/main/java/yadavsudhir405/rest/github/com/config/WebConfig.java
@@ -7,6 +7,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 /**
  * Created by sudhiry on 2/16/19.
  */
+
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
