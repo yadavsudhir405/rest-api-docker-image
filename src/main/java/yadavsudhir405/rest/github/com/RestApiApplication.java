1c1e8974b938f1c0423cf9d1dd9830e1c81b74d9
Sudhir Yadav <yadavsudhir405@gmail.com>
2019-03-09 22:19:32 +0530

    release plugin test

--- src/main/java/yadavsudhir405/rest/github/com/RestApiApplication.java
+++ src/main/java/yadavsudhir405/rest/github/com/RestApiApplication.java
@@ -1,14 +1,18 @@
 package yadavsudhir405.rest.github.com;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
 public class RestApiApplication  {
+	private static final Logger LOGGER = LoggerFactory.getLogger(RestApiApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestApiApplication.class, args);
+		LOGGER.info("Application Started");
 	}
 
 
